d4870c0826625072d7d609d0fbc1d8cb5e4e633a
Suyog Mishal <suyog15122@gmail.com>
2019-07-08 00:58:24 +0530

    react-app

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 class UserList extends React.Component{
-    constructor(props){
-        super(props);
-        console.log(props);
-
-    }
     render() {
         const {users} = this.props;
         return(<div>
